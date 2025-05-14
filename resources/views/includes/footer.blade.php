56e53ebab7e7c388c9ae8b3f3ff6fa5b6e3ea608
Siméon0477 <nouhoumimeon01@gmail.com>
2025-05-14 14:23:16 +0100

    Mise en état des styles du log in et du sign up

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,21 +5,21 @@
         <h2  class="text-primary">Liens Rapides</h2>
         <ul type="circle">
           <li><a href="">A propos de nous</a></li>
-          <li><a href="">Postes</a></li>
-          <li><a href="">Ajouter un poste</a></li>
-          <li><a href="">Se connecter</a></li>
+          <li><a href="{{route('posts.postsTable')}}">Postes</a></li>
+          <li><a href="{{route('posts.create')}}">Ajouter un poste</a></li>
+          <li><a href="{{route('register')}}">S'inscrire</a></li>
         </ul>
       </div>
       <div class="items">
           <h2 class="text-primary">Contacts</h2>
           <div class="icon">
-              <img src="{{ Vite::asset('resources/ico/ico (4).png') }}"> xxxxxxxxxxxxxxxxxxxxxxxxxx
+              <img src="{{ Vite::asset('resources/ico/ico (4).png') }}"> ecole nationale supérieure polytechnique de yaoundé
           </div>
           <div class="icon">
-              <img src="{{ Vite::asset('resources/ico/ico (6).png') }}"> +237 xxx xxx xxx, +237 xxx xxx xxx
+              <img src="{{ Vite::asset('resources/ico/ico (6).png') }}"> +237 696 553 621, +237 621 669 250
           </div>
           <div class="icon">
-              <img src="{{ Vite::asset('resources/ico/ico (5).png') }}"> kamermotion-art@gmail.com
+              <img src="{{ Vite::asset('resources/ico/ico (5).png') }}"> kamermotion_art@gmail.com
           </div>
           <div class="network">
               <a href="https://www.facebook.com/share/18UEPiARXo/" target="blank"><img src="{{ Vite::asset('resources/ico/ico (1).png') }}"></a>
